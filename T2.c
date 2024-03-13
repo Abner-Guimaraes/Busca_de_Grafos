67d6ab16c5351798ef486944a60bb2dc16bad1fa
AbnerAEDs <abner.guimaraes@sou.unifal-mg.edu.br>
2024-03-13 11:38:17 -0300

    testando arquivo.c

---
+++ T2.c
@@ -0,0 +1,18 @@
+/*------------------------------------------|
+|     Trabalho prático 2 - Busca de Grafos  |
+|                                           |
+|Discentes:Abner Gomes Guimarães            |
+|-------------------------------------------|
+*/
+
+
+int main(){
+
+
+
+
+
+
+
+    return 0;
+}
