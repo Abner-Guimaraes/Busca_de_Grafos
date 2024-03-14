2960416b560104da8d194db14c89ec301eb7662b
AbnerAEDs <abner.guimaraes@sou.unifal-mg.edu.br>
2024-03-14 11:18:30 -0300

    adicionadomakefile

--- T2.c
+++ T2.c
@@ -1,14 +1,23 @@
-/*------------------------------------------|
-|     Trabalho prático 2 - Busca de Grafos  |
-|                                           |
-|Discentes:Abner Gomes Guimarães            |
-|-------------------------------------------|
+/**+-----------------------------------------------------------+
+ | UNIFAL – Universidade Federal de Alfenas.                   |
+ | BACHARELADO EM CIENCIA DA COMPUTACAO.                       |
+ | Trabalho: Busca_de_Grafos                                   |
+ | Disciplina: Algoritmos e Estrutura de Dados II – Pratica    |
+ | Professor: Iago Augusto Carvalho                            |
+ | Aluno(s): Abner Gomes Guimarães - Felipe Araújo Correia     |
+ | Diogo da Silva Moreira - Gustavo Marcelino Izidoro          |
+ | Data: 14/03/2023                                            |
+ +-------------------------------------------------------------+
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
 
 
 int main(){
 
-
+printf("olá");
 
 
 
