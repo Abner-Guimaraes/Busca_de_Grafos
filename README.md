ac3aec730379e5804db8f88d9c74c8c257b1629e
AbnerAEDs <abner.guimaraes@sou.unifal-mg.edu.br>
2024-03-26 23:18:46 -0300

    Explicação sobre o primeiro códido

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 Trabalho de Grafos em estruturas de dados .c/c++
 
 "Para o nosso trabalho de Matriz de Adjacência em linguagem C, estamos criando um programa que representa um labirinto usando uma matriz. Cada célula da matriz corresponde a uma parte do labirinto e suas conexões são indicadas pelos valores na matriz. Nosso objetivo é desenvolver um código capaz de resolver problemas relacionados ao labirinto, como encontrar caminhos entre dois pontos ou verificar a existência de um caminho válido da entrada para a saída."
+
+# Primeiro código: Algoritmo BFS com Matriz de Adjacência
+
+Este repositório contém uma implementação em C do algoritmo BFS (Busca em Largura) utilizando uma matriz de adjacência. Este algoritmo é uma forma eficiente de encontrar o caminho mais curto em um grafo não ponderado.
+
+## Matriz de Adjacência:
+
+A matriz de adjacência é uma estrutura de dados frequentemente utilizada para representar grafos. Em um grafo não direcionado, ela é uma matriz quadrada onde cada linha e coluna representam um vértice e os elementos indicam se existe uma aresta entre os vértices correspondentes. Para grafos direcionados, a matriz pode ser assimétrica. Se o grafo for ponderado, os elementos da matriz podem armazenar o peso das arestas.
+
+## BFS (Busca em Largura):
+
+A busca em largura é um algoritmo de busca em grafos que explora todos os vértices em um mesmo nível antes de prosseguir para os vértices do próximo nível. Este algoritmo é implementado tipicamente com o auxílio de uma fila, que armazena os vértices a serem visitados. Começando de um vértice inicial, o BFS visita todos os vértices adjacentes a ele, marcando-os como visitados e os colocando na fila. Esse processo continua até que todos os vértices acessíveis sejam visitados.
+
+## Implementação:
+
+A implementação neste repositório demonstra como usar uma matriz de adjacência para representar um grafo e como aplicar o algoritmo BFS para percorrer o grafo e encontrar o caminho mais curto entre dois vértices.
+
+Sinta-se à vontade para explorar o código e contribuir para melhorias!
+
+# Segundo código: A discutir
+
+Provavelmente um algorimo em matriz de adjacência só que com outro método, como por exemplo algoritmo de Dijkstra
+
+
+
--- T2.c
+++ T2.c
@@ -3,69 +3,97 @@
 
 #define MAX_vet 10
 
-//Matriz de adjacencia com Busca de largura(BFS);
-//Djkstra;
-//Qual o melhor método para não ir para o caminho errado;
-//Começar em ponto especifico;
+// Matriz de adjacencia com Busca de largura(BFS);
+// Qual o melhor método para não ir para o caminho errado;
+// Começar em ponto especifico;
 
-typedef struct Grafo {
+typedef struct {
   int num_vertices;
-  int matriz_adj[MAX_vet][MAX_vet]; // Estrutura do grafo
+  int matriz_adj[MAX_vet][MAX_vet]; // Estrutura do grafo;
 } Grafo;
 
-// Função que vai pegar o labirinto em char e tranformar em matriz adjacente
-//  X == parede == 0 == caminho não percorrido;
-//  0 == caminho == 1 == caminho percorrido;
-//  E e S a ser discutido;
-void *iniciar_Matriz_adjacencia(Grafo *grafo, int num_vertices) {
-  grafo->num_vertices = num_vertices;
-  int matriz[MAX_vet][MAX_vet];
-  int linha = 0, coluna = 0;
+typedef struct {
+  int linha;
+  int coluna;
+} Coordenada; // struct para especificar o ponto de inicio e fim do labirinto;
 
-  //colocar a matriz de char
-  //caso tenha algum caractere do if entao na matriz é colocado 1 no indice !!!a mudar
-  if( matriz[linha][coluna] == 'E' || matriz[linha][coluna] == 'S' || matriz[linha][coluna] == '0'){
-    for(int i = 0; i< num_vertices; i++){
-      for(int j = 0; j< num_vertices; j++){
-        grafo->matriz_adj[i][j] = 1;
-      }
-    }
-  }
-    if(matriz[linha][coluna] == 'X'){
-  for(int  i = 0; i < num_vertices; i++){
-    for(int j = 0; j < num_vertices; j++){
-      grafo->matriz_adj[i][j] = 0;
-      }
-    }
-  }
-}
-// Função que faz a leitura do  arquivo e coloca caractere em uma matriz do tipo
-// char para ser utilizado na verificação de labirinto;
-char leitura_de_caracteres(FILE *arq, char labirinto[MAX_vet][MAX_vet]) {
+// criando estruturas para fila para utilizar o metodo BFS;
+typedef struct no {
+  int item;
+  struct no *proximo;
+} No;
+
+typedef struct fila {
+  No *inicio;
+  No *fim;
+} Fila;
+
+// Função que faz a leitura do  arquivo e cria a matriz de adjacencia;
+// é nessesario utilizar debugger para analisar essa função;
+void iniciar_matriz_adjacencia(Grafo *grafo, FILE *arq) {
   char caractere;
   int linha = 0, coluna = 0;
+  Coordenada coordenada_inicio;
+  Coordenada coordenada_fim;
 
   while ((caractere = fgetc(arq)) != EOF) {
     if (caractere != '\n') {
-      labirinto[linha][coluna] = caractere;
-      coluna++;
-    } else {
-      coluna = 0;
-      linha++;
+      if (caractere == 'E' || caractere == 'S' || caractere == '0') {
+        grafo->matriz_adj[linha][coluna] = 1;
+        coluna++;
+        if (caractere == 'E') {
+          coordenada_inicio.linha = linha;
+          coordenada_inicio.coluna = coluna;
+        }
+        if (caractere == 'S') {
+          coordenada_fim.linha = linha;
+          coordenada_fim.coluna = coluna;
+        }
+      } else if (caractere == 'X') {
+        grafo->matriz_adj[linha][coluna] = 0;
+        coluna++;
+      } else {
+        linha++;
+        coluna = 0;
+      }
     }
   }
 }
 
-int main(void) {
-  char labirinto[MAX_vet][MAX_vet];
+void iniciar_fila(Fila *fila) {
+  fila->inicio = NULL;
+  fila->fim = NULL;
+}
 
-  FILE *arq = fopen("labirinto.txt", "r");
+int lista_vazia(Fila *fila) { return (fila->inicio == NULL); }
+
+void *inserir_V_fila(Fila *fila, int item) {}
+No *remover_V_fila(Fila *fila) {}
+
+int main(void) {
+
+  Grafo *labirinto;
+
+  FILE *arq = fopen("labirinto1.txt", "r");
   if (arq == NULL) {
     printf("Erro ao abrir o arquivo!!!\n");
   } else {
     printf("Arquivo aberto para leitura!!!\n");
   }
 
+  labirinto = (Grafo *)malloc(sizeof(Grafo));
+  if (labirinto == NULL) {
+    printf("Erro ao alocar memoria na matriz\n");
+    fclose(arq);
+    return 1;
+  }
+
+  iniciar_matriz_adjacencia(labirinto, arq);
+
   printf("Hello World\n");
+
+  free(labirinto);
+  fclose(arq);
+
   return 0;
 }
